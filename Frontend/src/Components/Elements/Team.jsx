f174643ab82b2930e619e645f4280cf76de07f23
Ritik kumar <ritikkumar8@amityonline.com>
2024-11-30 20:12:41 +0530

    Home page is done

--- Frontend/src/Components/Elements/Team.jsx
+++ Frontend/src/Components/Elements/Team.jsx
@@ -1,5 +1,6 @@
 import TeamImg from "../img/rb.png";
-
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Team = () => {
   return (
@@ -13,23 +14,61 @@ const Team = () => {
           the decentralized data concept to the masses.
         </p>
       </div>
-      <div className="flex justify-center w-[100%] h-[400px] mt-[200px]  gap-[50px]"> 
-        <div className="flex justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
-          <div className="w-[200px] h-[200px] bg-white mt-[-100px] rounded-full">
+      <div className="flex justify-center w-[100%] h-[400px] mt-[200px]  gap-[50px]">
+        <div className=" justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
+          <div className="w-[200px] h-[200px] bg-white ml-[50px]  mt-[-100px] rounded-full">
             <img src={TeamImg} alt="" />
-            </div>
-                     
-        </div>  
-        <div className="flex justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
-          <div className="w-[200px] h-[200px] bg-white mt-[-100px] rounded-full">
+          </div>
+          <h3 className="ml-[100px] mt-3"> Ritik Kumar</h3>
+          <p className="ml-[90px] text-[#8B9467]">Founder & CEO</p>
+          <div className="flex gap-4 justify-center mt-[200px]">
+            <a href="https://github.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaGithub size={24} />
+            </a>
+            <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="https://x.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaXTwitter size={24} />
+            </a>
+          </div>
+        </div>
+        <div className=" justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
+          <div className="w-[200px] h-[200px] bg-white ml-[50px]  mt-[-100px] rounded-full">
             <img src={TeamImg} alt="" />
-            </div>          
-        </div>  
-        <div className="flex justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
-          <div className="w-[200px] h-[200px] bg-white mt-[-100px] rounded-full">
+          </div>
+          <h3 className="ml-[120px] mt-3">Sing ji</h3>
+          <p className="ml-[120px] text-[#8B9467]">CTO</p>
+          <div className="flex gap-4 justify-center mt-[200px]">
+            <a href="https://github.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaGithub size={24} />
+            </a>
+            <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="https://x.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaXTwitter size={24} />
+            </a>
+          </div>
+        </div>
+        <div className=" justify-center w-[300px] h-[390px] mt-2 rounded-[20px]  bg-[#222325]">
+          <div className="w-[200px] h-[200px] bg-white ml-[50px]  mt-[-100px] rounded-full">
             <img src={TeamImg} alt="" />
-            </div>          
-        </div>      
+          </div>
+          <h3 className="ml-[100px] mt-3">Abhishek Kumar</h3>
+          <p className="ml-[120px] text-[#8B9467]">Developer</p>
+          <div className="flex gap-4 justify-center mt-[200px]">
+            <a href="https://github.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaGithub size={24} />
+            </a>
+            <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="https://x.com/username" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+              <FaXTwitter size={24} />
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
